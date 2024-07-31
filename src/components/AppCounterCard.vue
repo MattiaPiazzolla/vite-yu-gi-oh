c2987c79c8b7f2189805a536d6a05b552e620c9c
MattiaPiazzolla <mattiapiazzolla@gmail.com>
2024-07-31 18:45:37 +0200

    [UPD] added card counter

---
+++ src/components/AppCounterCard.vue
@@ -0,0 +1,17 @@
+<script>
+import { store } from '../store';
+
+export default {
+    data() {
+        return {
+            store
+        }
+    },
+}
+</script>
+<template>
+    <span>{{ store.cardList.length }}</span>
+</template>
+<style lang="">
+    
+</style>
