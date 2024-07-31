89c422d0f0e0da6cf65d174bca66e880f57d26b7
MattiaPiazzolla <mattiapiazzolla@gmail.com>
2024-07-31 16:25:23 +0200

    [UPD] installed axios and imported the api in the project

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,29 @@
 <script>
 import AppHeader from './components/AppHeader.vue';
 import AppMain from './components/AppMain.vue';
+import { store } from './store.js'
+import axios from 'axios'
 
 export default{
   components: {
     AppHeader,
     AppMain,
-  }
+  },
+  created() {
+    this.getCardList()
+  },
+  methods: {
+    getCardList() {
+      axios.get(store.apiUrl).then((result) => {
+        store.cardList = result.data.data
+      })
+    }
+  },
+  data() {
+    return {
+      store,
+    }
+  },
 }
 </script>
 
