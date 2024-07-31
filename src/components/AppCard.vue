6e4c46c9ba10fd6fcb7eaa28a3221465377a87ee
MattiaPiazzolla <mattiapiazzolla@gmail.com>
2024-07-31 16:29:39 +0200

    [UPD] created and imported component AppCard

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -1,16 +1,12 @@
 <script>
-import AppCard from './AppCard.vue';
-
 export default {
-    components: {
-        AppCard
-    }
+    
 }
 </script>
 
 <template>
     <div>
-        <AppCard />
+        
     </div>
 </template>
 
